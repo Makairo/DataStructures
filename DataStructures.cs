3a9ee09b4ea15316763d374f7ea9c8052c5c7938
Andrew Hejl <andrew.hejl@outlook.com>
2021-05-29 15:25:14 -0700

    Add project files.

---
+++ DataStructures.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace DataStructures
+{
+    public class Node<T>
+    {
+        T data;
+        public Node<T> next;
+
+        public Node()
+        {
+            data = default(T);
+            next = null;
+        }
+
+        public Node(T x)
+        {
+            data = x;
+            next = null;
+        }
+
+        public void Append(T x)
+        {
+            if (next == null)
+            {
+                next = new Node<T>(x);
+            }
+            else
+            {
+                next.Append(x);
+            }
+        }
+
+
+
+        public Node<T> GetLast()
+        {
+            if (next == null)
+            {
+                return this;
+            }
+            else
+            {
+                return next.GetLast();
+            }
+        }
+
+        public int Count(int x = 0)
+        {
+            if (next == null)
+            {
+                return x;
+            }
+            else
+            {
+                x++;
+                return next.Count(x);
+            }
+        }
+
+        public void Print()
+        {
+
+            if (next != null)
+            {
+                Console.Write(" | " + data + " | " + "->");
+                next.Print();
+            }
+            else
+            {
+                Console.WriteLine(" | " + data + " | ");
+            }
+        }
+    }
+
+    public class LinkedList<T>
+    {
+        Node<T> HeadNode;
+        Node<T> LastNode;
+        int Count;
+
+        public LinkedList()
+        {
+            HeadNode = null;
+            LastNode = null;
+            Count = 0;
+        }
+
+        public LinkedList(T x)
+        {
+            HeadNode = new Node<T>(x);
+            LastNode = HeadNode;
+            Count = HeadNode.Count();
+        }
+
+        public void Append(T x)
+        {
+            if(HeadNode == null)
+            {
+                HeadNode = new Node<T>(x);
+                LastNode = HeadNode;
+            }
+            else
+            {
+                HeadNode.Append(x);
+                LastNode = HeadNode.GetLast();
+            }
+            Count = HeadNode.Count();
+        }
+
+        public void Prepend(T x)
+        {
+            if (HeadNode == null)
+            {
+                HeadNode = new Node<T>(x);
+                LastNode = HeadNode;
+            }
+            else
+            {
+                Node<T> temp = new Node<T>(x);
+                temp.next = HeadNode;
+                HeadNode = temp;
+            }
+            Count = HeadNode.Count();
+        }
+
+        public void Print()
+        {
+            if (HeadNode == null)
+            {
+                return;
+            }
+            HeadNode.Print();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> MyList = new LinkedList<int>();
+            LinkedList<string> MyStringList = new LinkedList<string>();
+
+
+            MyList.Append(54);
+            MyList.Append(56);
+            MyList.Append(67);
+            MyList.Append(78);
+            MyList.Print();
+
+
+            MyStringList.Append("Hello");
+            MyStringList.Append("my");
+            MyStringList.Append("name");
+            MyStringList.Append("is");
+            MyStringList.Append("Andrew");
+            MyStringList.Print();
+        }
+    }
+}
